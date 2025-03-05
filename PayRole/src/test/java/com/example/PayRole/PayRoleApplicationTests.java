980d564ddf90daa55bb446c024f8da0abba481d3
Divyanshu-rajpoot <divy.r.007@gmail.com>
2025-03-05 20:50:32 +0530

    [Divyanshu] Added the Base Project

---
+++ PayRole/src/test/java/com/example/PayRole/PayRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PayRole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayRoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
